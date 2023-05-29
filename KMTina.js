c91a071d4e24f85b2200ced0de2ef981ddfa0ba4
Shahid Amin CYF <82290321+Schboostie@users.noreply.github.com>
2023-05-29 16:24:57 +0100

    Create KMTina.js

---
+++ KMTina.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var gallery = new Flickity('.gallery', {
+    // Flickity options here
+    // You can customize the transition, autoplay, etc.
+    // Check Flickity documentation for available options
+    autoPlay: true,
+    wrapAround: true
+  });
+});
